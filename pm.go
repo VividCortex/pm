08e6b54f63d984de1261c14aa80308840debf318
Baron Schwartz <baron@xaprb.com>
2013-11-18 04:52:28 -0500

    Update documentation, add tests

--- pm.go
+++ pm.go
@@ -1,4 +1,81 @@
-// Package pm is a process manager with a TCP interface.
+// Copyright (c) 2013 VividCortex. Please see the LICENSE file for license terms.
+
+/*
+Package pm is a process manager with a TCP interface.
+
+A processlist is useful for inspecting and managing what's running in a
+program, such as an HTTP server or other server program. Processes within this
+program are user-defined tasks, such as an HTTP request.
+
+To use pm, you should make one call to SetCols() when you start your server
+running. This specifies the columns you want to see in printouts of the
+processlist. To continue the HTTP example:
+
+	pm.SetCols("host", "method", "uri")
+
+Follow this with a call to ListenAndServe():
+
+	go func() {
+		for {
+			log.Println(pm.ListenAndServe(":8081"))
+			time.Sleep(time.Second)
+		}
+	}()
+
+Then, when a process/task/request begins, you will call Start() with the
+process's ID and values for its columns, and defer a call to Done():
+
+	pm.Start(requestID, map[string]interface{}{
+		"host":   req.RemoteAddr,
+		"method": req.Method,
+		"uri":    req.RequestURI,
+	})
+	defer pm.Done(requestID)
+
+This is all you'll need to view a table of running requests and how long they've
+been executing. You can now connect to port 8081 and view the processlist with a
+tool such as netcat:
+
+	$ nc localhost 8081
+	id     status    time  host             method  uri
+	proc1  init    0.0000  127.0.0.1:18364  GET     /users/
+	proc2  init    0.0001  127.0.0.1:3527   GET     /users/5/friends
+
+If you add calls to pm.Status() throughout your code, you'll be able to see the
+status change, which can be helpful for troubleshooting:
+
+	pm.Status(requestId, "authenticating")
+
+Status changes also serve as an opportunity to optionally make processes
+killable. If you execute the return value of calls to Status(), then you can
+introduce a runtime panic into that goroutine as desired, with a specified
+message:
+
+	pm.Status(requestId, "authenticating")()
+
+The kill command and message can be specified through your netcat connection:
+
+	kill proc2 houston, we have a problem
+
+This will cause a panic with a stacktrace such as the following:
+
+	2013/11/17 15:39:29 http: panic serving 127.0.0.1:55780: process killed: "houston, we have a problem"
+	goroutine 6 [running]:
+	net/http.func·007()
+	    /usr/local/go/src/pkg/net/http/server.go:1022 +0xac
+	github.com/VividCortex/pm.func·001()
+	    /Users/baron/Documents/go/src/github.com/VividCortex/pm/pm.go:76 +0x4c
+
+This is safe to do with HTTP servers based on net/http as long as the call to
+Status() isn't made from a separate goroutine. The goroutine that net/http
+starts to serve every incoming request has a deferred recover(), which will
+catch and print out such panics without causing the entire server process to
+die.
+
+In addition to killing processes, you can control the refresh rate through your
+netcat session with the "delay" command, which takes a single argument that is
+parsed by time.ParseDuration().
+*/
 package pm
 
 import (
@@ -12,6 +89,9 @@ import (
 	"time"
 )
 
+// Proclist represents a list of processes and the columns associated with them.
+// Most people will not use Proclist or its methods directly, but will instead use
+// the package-level functions that act on the default package-level variable.
 type Proclist struct {
 	m     sync.Mutex
 	procs map[string]*Proc
@@ -19,10 +99,12 @@ type Proclist struct {
 	cols  []string       // The following columns are reserved: id, status, time
 }
 
+// SetCols sets the columns expected to be present for every process.
+// The id, status, and time columns are built-in.
 func (p *Proclist) SetCols(cols ...string) {
 	p.m.Lock()
 	defer p.m.Unlock()
-	p.cols = append(p.cols, cols...)
+	p.cols = append([]string{"id", "status", "time"}, cols...)
 	for _, col := range p.cols {
 		if p.lens[col] < len(col) {
 			p.lens[col] = len(col)
@@ -30,6 +112,9 @@ func (p *Proclist) SetCols(cols ...string) {
 	}
 }
 
+// Start creates and starts a new process. After this, the process's columns
+// do not change, with the exception of the status and time columns, which
+// are special.
 func (p *Proclist) Start(id string, cols map[string]interface{}) {
 	p.m.Lock()
 	defer p.m.Unlock()
@@ -51,6 +136,7 @@ func (p *Proclist) Start(id string, cols map[string]interface{}) {
 	p.lens["status"] = 6 // len("status")
 }
 
+// Done stops and removes a process from the list, and returns it.
 func (p *Proclist) Done(id string) *Proc {
 	p.m.Lock()
 	defer p.m.Unlock()
@@ -62,6 +148,16 @@ func (p *Proclist) Done(id string) *Proc {
 	return proc
 }
 
+// Status changes a process's status. Use this to indicate what the process is
+// doing. Status() doubles as a status update and a check to see if the
+// process has been killed. The return value is a function that will kill the
+// process via a runtime panic, if so instructed with Kill(). The correct way
+// to use Status() is to execute the function it returns:
+//
+//    pm.Status("myproc", "authenticating")()
+//
+// If you don't include the trailing parens, then the process won't be
+// killable at this point in its execution.
 func (p *Proclist) Status(id, status string) func() {
 	p.m.Lock()
 	defer p.m.Unlock()
@@ -80,6 +176,7 @@ func (p *Proclist) Status(id, status string) func() {
 	return func() { panic("no such process " + id) }
 }
 
+// Kill schedules the process for killing at the next Status().
 func (p *Proclist) Kill(id, message string) error {
 	p.m.Lock()
 	defer p.m.Unlock()
@@ -90,6 +187,7 @@ func (p *Proclist) Kill(id, message string) error {
 	return ProcessNotFound
 }
 
+// Contents returns an io.Reader containing the formatted processlist.
 func (p *Proclist) Contents() io.Reader {
 	var (
 		b       bytes.Buffer
@@ -127,6 +225,14 @@ func (p *Proclist) Contents() io.Reader {
 	return &b
 }
 
+// ListenAndServe() listens on a TCP socket. Connections to this socket get repeating
+// printouts of the processlist at 5-second intervals. Connections can send commands
+// as well. The command syntax is:
+//
+//    kill <id> <message>
+//       Schedules the specified process to be killed, with the given message.
+//    delay <duration>
+//       Changes the refresh interval. The argument is parsed with time.ParseDuration().
 func (p *Proclist) ListenAndServe(addr string) error {
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -138,8 +244,7 @@ func (p *Proclist) ListenAndServe(addr string) error {
 			return err
 		}
 
-		// Start a command-listener on this connection. It will look for
-		// commands and execute them. (kill <id> <msg>, delay <duration>)
+		// Start a command-listener on this connection.
 		go func() {
 			defer c.Close()
 			delay := time.Second * 3
@@ -189,6 +294,7 @@ func (p *Proclist) ListenAndServe(addr string) error {
 	}
 }
 
+// Proc represents a process.
 type Proc struct {
 	status, kill   string
 	start, updated time.Time
@@ -196,6 +302,7 @@ type Proc struct {
 	cols           map[string]interface{}
 }
 
+// Status updates the process's status and keeps track of the time spent in each status.
 func (p *Proc) Status(status string, t time.Time) {
 	p.profile[p.status] = p.profile[p.status] + t.Sub(p.updated)
 	p.status = status
@@ -215,30 +322,56 @@ func init() {
 	}
 }
 
+// SetCols sets the columns expected to be present for every process.
+// The id, status, and time columns are built-in.
 func SetCols(cols ...string) {
 	pl.SetCols(cols...)
 }
 
+// Start creates and starts a new process. After this, the process's columns
+// do not change, with the exception of the status and time columns, which
+// are special.
 func Start(id string, cols map[string]interface{}) {
 	pl.Start(id, cols)
 }
 
+// Done stops and removes a process from the list, and returns it.
 func Done(id string) *Proc {
 	return pl.Done(id)
 }
 
+// Status changes a process's status. Use this to indicate what the process is
+// doing. Status() doubles as a status update and a check to see if the
+// process has been killed. The return value is a function that will kill the
+// process via a runtime panic, if so instructed with Kill(). The correct way
+// to use Status() is to execute the function it returns:
+//
+//    pm.Status("myproc", "authenticating")()
+//
+// If you don't include the trailing parens, then the process won't be
+// killable at this point in its execution.
 func Status(id, status string) func() {
 	return pl.Status(id, status)
 }
 
+// Kill schedules the process for killing at the next Status().
 func Kill(id, message string) error {
 	return pl.Kill(id, message)
 }
 
+// Contents returns an io.Reader containing the formatted processlist.
 func Contents() io.Reader {
 	return pl.Contents()
 }
 
+// ListenAndServe() listens on a TCP socket. Connections to this socket get repeating
+// printouts of the processlist at 5-second intervals. Connections can send commands
+// as well. The command syntax is:
+//
+//    kill <id> <message>
+//       Schedules the specified process to be killed, with the given message.
+//    delay <duration>
+//       Changes the refresh interval. The argument is parsed with time.ParseDuration().
 func ListenAndServe(addr string) error {
 	return pl.ListenAndServe(addr)
 }
--- pm_test.go
+++ pm_test.go
@@ -1,20 +1,22 @@
+// Copyright (c) 2013 VividCortex. Please see the LICENSE file for license terms.
 package pm
 
 import (
 	"fmt"
+	"net"
 	"strings"
 	"testing"
+	"time"
 )
 
-func Test1(t *testing.T) {
+func TestBasicFunctionality(t *testing.T) {
 	SetCols("col1", "long_col")
 	Start("proc1", map[string]interface{}{"col1": 123, "long_col": "the quick brown fox jumped over the lazy red dog"})
 	defer Done("proc1")
 	Start("proc2", map[string]interface{}{"col1": 345, "long_col": "it was the best of times, it was the worst of times"})
 	defer Done("proc2")
 	Status("proc2", "wait")
-	r := Contents()
-	c := fmt.Sprint(r)
+	c := fmt.Sprint(Contents())
 	t.Log("\n" + c)
 	if !strings.Contains(c, "proc2  wait") {
 		t.Error("proc2 isn't wait")
@@ -24,5 +26,55 @@ func Test1(t *testing.T) {
 	}
 	if !strings.Contains(c, "proc2  wait        0") {
 		t.Error("bad alignment on proc2  wait        0")
+	}
+
+	Done("proc2")
+	c = fmt.Sprint(Contents())
+	t.Log("\n" + c)
+	if strings.Contains(c, "proc2") {
+		t.Error("proc2 still exists")
+	}
+
+	var msg string
+	Kill("proc1", "houston, we have a problem")
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				msg = fmt.Sprint(r)
+			}
+		}()
+		Status("proc1", "this should die")()
+	}()
+	time.Sleep(time.Second) // wait for the panic
+	if !strings.Contains(msg, "houston, we have a problem") {
+		t.Error("kill wasn't successful")
+	}
+
+}
+
+func TestNetListener(t *testing.T) {
+	SetCols("col1", "long_col")
+	Start("proc1", map[string]interface{}{"col1": 123, "long_col": "the quick brown fox jumped over the lazy red dog"})
+
+	go func() {
+		t.Log(ListenAndServe(":9999"))
+	}()
+	time.Sleep(time.Second) // give it time to start listening
+
+	c, err := net.Dial("tcp", ":9999")
+	if err != nil {
+		t.Error(err)
+	}
+	defer c.Close()
+	buf := make([]byte, 16*1024)
+	n, err := c.Read(buf)
+	if n > 0 {
+		list := string(buf[:n])
+		t.Log("\n" + list)
+		if !strings.Contains(list, "quick brown fox") {
+			t.Error("didn't get quick brown fox from net listener")
+		}
+	} else if err != nil {
+		t.Error(err)
 	}
 }
